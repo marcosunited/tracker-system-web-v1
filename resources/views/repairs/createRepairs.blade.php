6e15361f4c4a70df2a2e5383830efcb7f3db88fe
ULS IT <67090139+it-united-lifts@users.noreply.github.com>
2020-11-02 14:46:24 +1100

    1.0.3.2
    Fix bug on creating new repairs
    
    Co-Authored-By: Javier Ladino <javilad91@gmail.com>

--- resources/views/repairs/createRepairs.blade.php
+++ resources/views/repairs/createRepairs.blade.php
@@ -26,65 +26,94 @@
 <script src="{{ asset('js/plugins/moment/locales.min.js') }}"></script>
 <script src="{{ asset('js/plugins/datetimepicker/datetime.min.js') }}"></script>
 <script>
-    jQuery(function () {
+    jQuery(function() {
         $('#datetimepicker1').datetimepicker({
             format: 'YYYY-MM-DD HH:mm:ss',
             value: new Date()
         });
-});  
+    });
 </script>
 <script>
-    $(document).ready(function() { $(".job_select").select2(); });
-    $(document).ready(function() { $(".tech_select").select2(); });
-    $(document).ready(function() { $(".lift_select").select2({
-        multiple: true,
-    }); });
-    $(document).ready(function() { $(".fault_select").select2(); });
- </script>
+    $(document).ready(function() {
+        $(".job_select").select2();
+    });
+    $(document).ready(function() {
+        $(".tech_select").select2();
+    });
+    $(document).ready(function() {
+        $(".lift_select").select2({
+            multiple: true,
+        });
+    });
+    $(document).ready(function() {
+        $(".fault_select").select2();
+    });
+</script>
 
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <script>
-    $(document).ready(function(){
-        $(".lift_select").select2({ multiple:true});
+    $(document).ready(function() {
+        $(".lift_select").select2({
+            multiple: true
+        });
         var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-        $(".job_select").change(function(){
+        $(".job_select").change(function() {
             $.ajax({
                 /* the route pointing to the post function */
                 url: '/callouts/selectedJob',
                 type: 'POST',
                 /* send the csrf-token and the input to the controller */
-                data: {_token: CSRF_TOKEN, 
-                        message:$(".job_select").select2("val")},
-                dataType: 'JSON',
-                success:function(data){
-                    $("#lift_select").select2().empty()                  
-                    
-                    for (var i=0;i<=data.length;i++) {                        
-                        var html='<option value="'+data[i]['id']+'" selected>'+data[i]['lift_name']+'</option>';           
-                        $("#lift_select").append(html);             
-                    }        
-                    $("#lift_select").select2({ multiple:true});                 
+                data: {
+                    _token: CSRF_TOKEN,
+                    message: $(".job_select").select2("val")
                 },
-                error:function() {
+                dataType: 'JSON',
+                success: function(data) {
+
+                    if (data) {
+                        if (data['lifts']) {
+                            var lifts = data['lifts'];
+
+                            $("#lift_select").select2().empty()
+
+                            for (var i = 0; i < lifts.length; i++) {
+                                var html = '<option value="' + lifts[i]['id'] + '" selected>' + lifts[i]['lift_name'] + '</option>';
+                                $("#lift_select").append(html);
+                            }
+                            $("#lift_select").select2({
+                                multiple: true
+                            });
+                        }
+                    }
+                },
+                error: function() {
 
                 }
-            }); 
-            
+            });
+
             //Session::put('selectedJob',$(".job_select").select2("val"));
             //$selectedJob = Session::get('selectedJob');
             //window.location.reload(true);
-            $( "#here" ).load(window.location.href + " #here" );
+            $("#here").load(window.location.href + " #here");
             //$( "#there" ).load(window.location.href + " #there" );           
         });
-   });    
+    });
 </script>
 
 <!-- Page JS Helpers (BS Notify Plugin) -->
 
-<script>jQuery(function(){ Dashmix.helpers('notify'); });</script>
+<script>
+    jQuery(function() {
+        Dashmix.helpers('notify');
+    });
+</script>
 
 <!-- Page JS Helpers (BS Datepicker + BS Colorpicker + BS Maxlength + Select2 + Ion Range Slider + Masked Inputs plugins) -->
-<script>jQuery(function(){ Dashmix.helpers(['datepicker', 'colorpicker', 'maxlength', 'select2', 'rangeslider', 'masked-inputs']); });</script>
+<script>
+    jQuery(function() {
+        Dashmix.helpers(['datepicker', 'colorpicker', 'maxlength', 'select2', 'rangeslider', 'masked-inputs']);
+    });
+</script>
 @endsection
 
 @section('content')
@@ -111,7 +140,7 @@
                 </h2>
                 <div class="row push">
                     <div class="col-lg-4" style="padding-left: 50px;">
-                    <div class="form-group">
+                        <div class="form-group">
                             <label for="example-text-input">Job Name</label>
                             <select class="form-control job_select" name="job_id" required>
 
@@ -130,8 +159,7 @@
                         <div class="form-group">
                             <label for="example-text-input">Repair Time</label>
                             <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
-                                <input type="text" name="repair_time" class="form-control datetimepicker-input"
-                                    data-target="#datetimepicker1" />
+                                <input type="text" name="repair_time" class="form-control datetimepicker-input" data-target="#datetimepicker1" />
                                 <div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
                                     <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                                 </div>
@@ -142,10 +170,10 @@
                             <input type="text" class="form-control" placeholder="" name="quote_no" required>
                         </div>
                         <div class="col-lg-8 col-xl-6" style="padding-left: 230px;">
-                    </div>
+                        </div>
                     </div>
                     <div class="col-lg-8 col-xl-6" style="padding-left: 230px;">
-                    <div class="form-group">
+                        <div class="form-group">
                             <label for="example-text-input">Current Status</label>
                             <select class="form-control" name="repair_status_id" required>
                                 <option value="">----------Select Status-----------</option>
